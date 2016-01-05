ebb7d5b8960395405deed25391c18b8650287168
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-01-04 18:16:34 -0700

    hello world loop working

--- http_iteration0.rb
+++ http_iteration0.rb
@@ -1,33 +1,42 @@
 require 'socket'
 #listens on port 9292
-tcp_server = TCPServer.new(9292)
-client = tcp_server.accept
 
-puts "Ready for a request"
-#Read the request from the client object in an IO stream. Store all requests in an array called request_lines
-request_lines = []
-while line = client.gets and !line.chomp.empty?
-  #when program runs it'll hang on gets method waiting for request to come in
-  #when message arrives gets read and stored into request_lines. prints to console
-  request_lines << line.chomp
+
+  tcp_server = TCPServer.new(9292)
+  client = tcp_server.accept
+
+@count = 0
+
+loop do
+@count += 1
+  puts "Ready for a request"
+  #Read the request from the client object in an IO stream. Store all requests in an array called request_lines
+  request_lines = []
+  while
+    line = client.gets and !line.chomp.empty?
+    #when program runs it'll hang on gets method waiting for request to come in
+    #when message arrives gets read and stored into request_lines. prints to console
+    request_lines << line.chomp
+  end
+
+  puts "Got this request:"
+  puts request_lines.inspect
+
+  #build response
+  puts "Sending response."
+
+  response = "<pre>" + request_lines.join("\n") + "</pre>"
+  output = "<html><head></head><body>Hello, World! (#{@count})</body></html>"
+  headers = ["http/1.1 200 ok",
+            "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
+            "server: ruby",
+            "content-type: text/html; charset=iso-8859-1",
+            "content-length: #{output.length}\r\n\r\n"].join("\r\n")
+  client.puts headers
+  client.puts output
 end
 
-puts "Got this request:"
-puts request_lines.inspect
 
-#build response
-puts "Sending response."
-@count = 0
-response = "<pre>" + request_lines.join("\n") + "</pre>"
-output = "<html><head></head><body>Hello, World! (#{@count})</body></html>"
-headers = ["http/1.1 200 ok",
-          "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
-          "server: ruby",
-          "content-type: text/html; charset=iso-8859-1",
-          "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-client.puts headers
-client.puts output
-@count += 1
 
 #close server
 puts ["Wrote this response:", headers, output].join("\n")
