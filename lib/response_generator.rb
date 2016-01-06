74b6776557ff19cb12cbcfa589df3c3237003e92
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-01-05 20:47:49 -0700

    hello counter method ok

--- lib/response_generator.rb
+++ lib/response_generator.rb
@@ -1,24 +1,29 @@
-
+require 'pry'
 
 class ResponseGenerator
 
 
-  def iterator0_result(request, counter = 1)
+  def iterator0_result(request, counter = 0)
+    if request.join.split[1] == "/hello" then return hello(counter)
+    else return diagnostics(request)
+    end
+  end
 
-
+  def hello(counter)
     "<html><head></head><body>HELLO WORLD(#{counter})</body></html>"
   end
 
-  def iterator1_result(request)
-    result = ["Verb: #{request[0].split(" ")[0]}",
+  def diagnostics(request)
+    result = ["Verb: #{request[0].split(" ")[0]}"/n,
     "Path: #{request[0].split(" ")[1]}",
     "Protocol: #{request[0].split(" ")[2]}",
     "#{request[1]}",
-    "Port: #{request[1][-4] + request[1][-3] + request[1][-2] + request[1][-1]}",
-    "Origin: request[1].split(" ")[1].split(" : ")[0]"
-    
+    "Port: #{request[1][-4]}#{request[1][-3]}#{request[1][-2]}#{request[1][-1]}",
+    "Origin: #{request[1].split(" ")[1].split(":")[0]}"]
 
-   ]
+    binding.pry
+
+
 
    #.chomp deletes backslash
 
