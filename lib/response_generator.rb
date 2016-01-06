da1d70266229a2794bec113e57ac7fe69ce2ea6c
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-01-06 09:02:15 -0700

    through iteration 2

--- lib/response_generator.rb
+++ lib/response_generator.rb
@@ -5,6 +5,8 @@ class ResponseGenerator
 
   def iterator0_result(request, counter = 0)
     if request.join.split[1] == "/hello" then return hello(counter)
+    elsif request.join.split[1] == "/datetime" then return datetime
+    elsif request.join.split[1] == "/shutdown" then return shutdown(counter)
     else return diagnostics(request)
     end
   end
@@ -14,21 +16,29 @@ class ResponseGenerator
   end
 
   def diagnostics(request)
-    result = ["Verb: #{request[0].split(" ")[0]}"/n,
+    result = ["Verb: #{request[0].split(" ")[0]}",
     "Path: #{request[0].split(" ")[1]}",
     "Protocol: #{request[0].split(" ")[2]}",
     "#{request[1]}",
     "Port: #{request[1][-4]}#{request[1][-3]}#{request[1][-2]}#{request[1][-1]}",
-    "Origin: #{request[1].split(" ")[1].split(":")[0]}"]
+    "Origin: #{request[1].split(" ")[1].split(":")[0]}", "Accept:#{request[6].split(":")[1]+request[8]+request[11].split(";")[1]}"]
+    result = result.map do |line|
+      puts line
+    end
 
-    binding.pry
-
-
-
-   #.chomp deletes backslash
-
-
+  def datetime
+    t = Time.new
+    t.strftime("%l:%M%p on %A, %B %e, %Y")
   end
+
+  def shutdown(counter)
+    "Total Requests: #{counter}"
+  end
+
+
+
+
+
 
 end
   # puts "Got this request:"
